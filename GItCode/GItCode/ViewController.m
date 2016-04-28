78385fa601d2e1101fb2ec746e8846cf2db7b492
qianfeng <473098092@qq.com>
2016-04-28 11:20:46 +0800

    打印

---
+++ GItCode/GItCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GItCode
+//
+//  Created by qianfeng on 16/4/28.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello Word!!!!!");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
